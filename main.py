ba64d5f8d44a5815d5907184e29e7527003d073e
niticastefan <45498332+niticastefan@users.noreply.github.com>
2021-03-12 15:48:16 +0200

    Update and rename main.py to kivy_hw.py

--- main.py
+++
@@ -1,8 +0,0 @@
-echo "# KivyHelloWorld" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/niticastefan/KivyHelloWorld.git
-git push -u origin main
-                
